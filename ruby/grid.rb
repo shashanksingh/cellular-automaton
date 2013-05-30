68a87f0bc906983b177e258032a9d30e556725b0
shashank singh <shashank.personal@gmail.com>
2013-05-30 17:06:29 +0530

    yay now we know global variable are in $ and cellular automaton has now good start bevcause its generic enough

--- ruby/grid.rb
+++ ruby/grid.rb
@@ -1,4 +1,4 @@
-grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+$grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 def print_grid(grid)
@@ -12,7 +12,7 @@ end
 
 
 def find_binary_representation_of_a_cell(grid,x,y)
-	return [1,1,1]
+	return [grid[x-1][y],grid[x][y],grid[x][y+1]]
 end
 
 def calculate_elementary_cellular_automaton_result(binary_representation)
@@ -20,18 +20,20 @@ def calculate_elementary_cellular_automaton_result(binary_representation)
 end 
 
 def calculate_automaton(grid)
-	grid.each do |x|
-		x.each do |y|
+	x = 0
+	grid.each do |x_array|
+		x_array.each do |y|
 			return_value = find_binary_representation_of_a_cell(grid,x,y)
-			calculate_elementary_cellular_automaton_result(return_value)
+			$grid[x][y] = calculate_elementary_cellular_automaton_result(return_value)
 		end
+		x += 1
 	end	
 end
 
 
 puts "input grid"
-print_grid(grid)
+print_grid($grid)
 puts "output grid"
-return_grid = calculate_automaton(grid)
+return_grid = calculate_automaton($grid)
 print_grid(return_grid)
 

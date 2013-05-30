2894e322b80800e6b086e8eb5189beb1e2bb303a
shashank singh <shashank.personal@gmail.com>
2013-05-30 16:34:59 +0530

    adding more functionality to solve elementarry cellular automatons

--- ruby/grid.rb
+++ ruby/grid.rb
@@ -11,7 +11,14 @@ def print_grid(grid)
 end
 
 
+def calculate_automaton(grid)
+	return [[1,1,1]]
+end
 
 
+puts "input grid"
 print_grid(grid)
+puts "output grid"
+return_grid = calculate_automaton(grid)
+print_grid(return_grid)
 

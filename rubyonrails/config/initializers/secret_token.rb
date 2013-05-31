6222277e62e994d7a17b5f81a27721925670d385
shashank singh <shashank.personal@gmail.com>
2013-05-31 17:32:12 +0530

    added bare bone strucutre for ruby on rails application

---
+++ rubyonrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyonrails::Application.config.secret_token = '0ebefcf09344accb40b815fc39fe105195f141bcf3695b17df86bf0ac52767e5af37e51497bae1f55947bdd17d2a942ec6787d231ed20e9673073371bd9539bb'

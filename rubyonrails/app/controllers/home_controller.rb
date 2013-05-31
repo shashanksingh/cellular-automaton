4c7ad22365d6d672f31de7e2d8443ea426d7c919
shashank singh <shashank.personal@gmail.com>
2013-05-31 18:53:27 +0530

    added grid

--- rubyonrails/app/controllers/home_controller.rb
+++ rubyonrails/app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	@grid = Automaton.last
+  	respond_to do |format|
+  		format.html #render index.html.erb
+  		format.xml { render :xml => @posts }
+  	end
   end
 end
